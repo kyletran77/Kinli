0da9a205efb4c340882c6bc106bd932a3bd91f2f
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-06-06 01:52:17 +0530

    refactor: segregated components, code cleanup

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,9 +1,18 @@
+import Post from "components/Post";
 import TextEditor from "components/TextEditor";
+import { useSelector } from "react-redux";
 
 export default function Explore() {
+  const { allPosts } = useSelector((state) => state.allPosts);
+
   return (
-    <div className="flex bg-red-200 w-full">
+    <div className="flex flex-col w-full">
       <TextEditor />
+      <div>
+        {allPosts?.map((post) => (
+          <Post post={post} key={post.postID} />
+        ))}
+      </div>
     </div>
   );
 }
