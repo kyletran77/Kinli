dfe243c230609eedf2421030c1a36c0625959530
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-06-06 20:18:41 +0530

    fix: avatar aspect ratio

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,13 +5,17 @@ import { deleteComment } from "../firebase/firebase-calls";
 export default function Comment({ comment, post }) {
   const currentUser = auth.currentUser;
   return (
-    <div className="flex flex-col bg-blue-50 my-1 p-2 rounded-md relative gap-1">
+    <div className="flex flex-col bg-blue-50 my-1 p-2 rounded-md relative">
       <div className="flex gap-2">
-        <img src={comment.avatar} alt="commenter-dp" className="h-6" />
+        <img
+          src={comment.avatar}
+          alt="commenter-dp"
+          className="h-7 aspect-square rounded-full"
+        />
         <p className="text-sm font-semibold">{comment.displayName}</p>
       </div>
       <p className="ml-8 text-gray-700">{comment.comment}</p>
-      <p className="ml-8 text-gray-500 text-xs ">{comment.date}</p>
+      <p className="ml-8 text-gray-500 text-xs mt-1">{comment.date}</p>
 
       {comment.userID === currentUser.uid && (
         <button
