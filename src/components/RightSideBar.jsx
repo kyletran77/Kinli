8df39e18e18d438daf8d9dc0859c3115bb11cca8
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-06-06 20:33:27 +0530

    fix: followers suggestions

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -1,75 +1,38 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function RightSideBar() {
+  const { allUsers } = useSelector((state) => state.allUsers);
+
   return (
     <div className="bg-slate-100 p-4 mx-2 w-[30rem]">
       <div className="flex flex-col gap-2 bg-gray-50 p-4 max-w-xs mx-auto rounded-md shadow-md">
         <h1>Suggestions</h1>
         <ul className="flex flex-col gap-3">
-          <li className="flex gap-1 items-center">
-            <img
-              src="https://pngset.com/images/female-user-image-icon-food-logo-symbol-trademark-transparent-png-839698.png"
-              alt="user-dp"
-              className="h-9 rounded-full"
-            />
-            <div className="flex items-center justify-between w-full">
-              <div>
-                <p className="text-sm font-semibold">Regina Phalange</p>
-                <p className="text-xs text-gray-500">@iykyk</p>
+          {allUsers.map((user) => (
+            <li className="flex gap-1 items-center my-1">
+              <img
+                src={
+                  user?.avatar
+                    ? user.avatar
+                    : "http://cdn.onlinewebfonts.com/svg/img_264570.png"
+                }
+                alt="user-dp"
+                className="h-9 aspect-square rounded-full"
+              />
+              <div className="flex items-center justify-between w-full">
+                <div>
+                  <p className="text-sm font-semibold">{user.displayName}</p>
+                  <p className="text-xs text-gray-500">
+                    @{user.email.split("@")[0]}
+                  </p>
+                </div>
+                <button className="border-2 border-blue-400 rounded-md px-2">
+                  Follow
+                </button>
               </div>
-              <button className="border-2 border-blue-400 rounded-md px-2">
-                Follow
-              </button>
-            </div>
-          </li>
-          <li className="flex gap-1 items-center">
-            <img
-              src="https://pngset.com/images/female-user-image-icon-food-logo-symbol-trademark-transparent-png-839698.png"
-              alt="user-dp"
-              className="h-9 rounded-full"
-            />
-            <div className="flex items-center justify-between w-full">
-              <div>
-                <p className="text-sm font-semibold">Regina Phalange</p>
-                <p className="text-xs text-gray-500">@iykyk</p>
-              </div>
-              <button className="border-2 border-blue-400 rounded-md px-2">
-                Follow
-              </button>
-            </div>
-          </li>
-          <li className="flex gap-1 items-center">
-            <img
-              src="https://pngset.com/images/female-user-image-icon-food-logo-symbol-trademark-transparent-png-839698.png"
-              alt="user-dp"
-              className="h-9 rounded-full"
-            />
-            <div className="flex items-center justify-between w-full">
-              <div>
-                <p className="text-sm font-semibold">Regina Phalange</p>
-                <p className="text-xs text-gray-500">@iykyk</p>
-              </div>
-              <button className="border-2 border-blue-400 rounded-md px-2">
-                Follow
-              </button>
-            </div>
-          </li>
-          <li className="flex gap-1 items-center">
-            <img
-              src="https://pngset.com/images/female-user-image-icon-food-logo-symbol-trademark-transparent-png-839698.png"
-              alt="user-dp"
-              className="h-9 rounded-full"
-            />
-            <div className="flex items-center justify-between w-full">
-              <div>
-                <p className="text-sm font-semibold">Regina Phalange</p>
-                <p className="text-xs text-gray-500">@iykyk</p>
-              </div>
-              <button className="border-2 border-blue-400 rounded-md px-2">
-                Follow
-              </button>
-            </div>
-          </li>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
