cb181a147d525caf523d08ef2100408a9dad9f17
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-06-09 22:33:01 +0530

    feat: added loaders and toasts

--- src/firebase/firebase-calls.js
+++ src/firebase/firebase-calls.js
@@ -1,3 +1,8 @@
+import { login, updateDP } from "features/user/userSlice";
+import { signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import toast from "react-hot-toast";
+import { auth, db, storage } from "./firebase";
 import {
   addDoc,
   arrayRemove,
@@ -8,7 +13,6 @@ import {
   getDoc,
   setDoc,
 } from "firebase/firestore";
-import { db } from "./firebase";
 
 const allPostsCollection = collection(db, "allPosts");
 const usersCollection = collection(db, "users");
@@ -21,104 +25,206 @@ const date = new Date().toLocaleDateString("en-IN", {
   second: "2-digit",
 });
 
+export const userLogin = async (
+  email,
+  password,
+  dispatch,
+  lastLocation,
+  navigate
+) => {
+  try {
+    const loader = toast.loading("Signing you in..");
+    const userAuth = await signInWithEmailAndPassword(auth, email, password);
+    dispatch(
+      login({
+        email: userAuth.user.email,
+        uid: userAuth.user.uid,
+        displayName: userAuth.user.displayName,
+        photoURL: userAuth.user.photoURL,
+      })
+    );
+    localStorage.setItem("authToken", userAuth.user.accessToken);
+    toast.success(`Welcome back, ${userAuth.user.displayName}!`, {
+      id: loader,
+    });
+    navigate(lastLocation);
+  } catch (error) {
+    toast.error("Couldn't log you in!");
+    console.log(error.message);
+  }
+};
+
 export const getUser = async (user, setUserData) => {
-  const userDoc = doc(usersCollection, user?.uid);
-  const docSnap = await getDoc(userDoc);
-  if (docSnap.exists()) {
-    setUserData(docSnap.data());
-  } else {
-    console.log("Could not retrieve user data");
+  try {
+    const userDoc = doc(usersCollection, user?.uid);
+    const docSnap = await getDoc(userDoc);
+    if (docSnap.exists()) {
+      setUserData(docSnap.data());
+    } else {
+      console.log("Could not retrieve user data");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const profileUpdate = async (
+  userInfo,
+  userData,
+  currentUser,
+  dispatch
+) => {
+  await setDoc(
+    doc(db, "users", currentUser?.uid),
+    {
+      coverPic: userInfo?.coverPic
+        ? userInfo?.coverPic
+        : userData.coverPic ?? "",
+      avatar: userInfo?.avatar ? userInfo?.avatar : userData.avatar,
+      bio: userInfo?.bio ? userInfo?.bio : userData.bio ?? "",
+      website: userInfo?.website ? userInfo?.website : userData.website ?? "",
+    },
+    { merge: true }
+  );
+  await updateProfile(currentUser, {
+    photoURL: userData.avatar,
+  });
+  dispatch(updateDP(userData.avatar));
+};
+
+export const uploadImage = async (user, file) => {
+  try {
+    const path = `images/${user?.uid}/${file?.name}`;
+    const imageRef = ref(storage, path);
+    const response = file && (await uploadBytesResumable(imageRef, file));
+    const pathName = response?.ref;
+    const url = await getDownloadURL(pathName);
+    return url;
+  } catch (error) {
+    toast.error("Could not upload image!");
+    console.log(error.message);
   }
 };
 
 export const createPost = async (user, post) => {
-  const postDoc = await addDoc(allPostsCollection, {
-    author: user.displayName,
-    uid: user.uid,
-    photoURL: user.photoURL,
-    caption: post.caption,
-    createdAt: new Date().toLocaleString(),
-    imageURL: post.imageURL,
-    likes: [],
-    comments: [],
-  });
+  try {
+    const loader = toast.loading("Posting...");
+    const postDoc = await addDoc(allPostsCollection, {
+      author: user.displayName,
+      uid: user.uid,
+      photoURL: user.photoURL,
+      caption: post.caption,
+      createdAt: new Date().toLocaleString(),
+      imageURL: post.imageURL,
+      likes: [],
+      comments: [],
+    });
 
-  await setDoc(
-    doc(allPostsCollection, postDoc.id),
-    {
-      postID: postDoc.id,
-    },
-    { merge: true }
-  );
+    await setDoc(
+      doc(allPostsCollection, postDoc.id),
+      {
+        postID: postDoc.id,
+      },
+      { merge: true }
+    );
+    toast.success("Post sent.", { id: loader });
+  } catch (error) {
+    toast.error("Post not sent. Try again!");
+  }
 };
 
 export const editPost = async (post, updatePost) => {
-  await setDoc(
-    doc(allPostsCollection, post.postID),
-    {
-      caption: updatePost.caption,
-      imageURL: updatePost.imageURL,
-    },
-    { merge: true }
-  );
+  try {
+    await setDoc(
+      doc(allPostsCollection, post.postID),
+      {
+        caption: updatePost.caption,
+        imageURL: updatePost.imageURL,
+      },
+      { merge: true }
+    );
+  } catch (error) {
+    toast.error("Couldn't update post. Try again!");
+  }
 };
 
 export const deletePost = async (post) => {
-  await deleteDoc(doc(allPostsCollection, post.postID));
+  try {
+    await deleteDoc(doc(allPostsCollection, post.postID));
+    toast.success("Post deleted");
+  } catch (error) {
+    toast.error("Couldn't delete post. Try again!");
+  }
 };
 
 export const likePost = async (postID, user) => {
-  await setDoc(
-    doc(allPostsCollection, postID),
-    {
-      likes: arrayUnion({
-        avatar: user.photoURL,
-        displayName: user.displayName,
-        userID: user.uid,
-      }),
-    },
-    { merge: true }
-  );
+  try {
+    await setDoc(
+      doc(allPostsCollection, postID),
+      {
+        likes: arrayUnion({
+          avatar: user.photoURL,
+          displayName: user.displayName,
+          userID: user.uid,
+        }),
+      },
+      { merge: true }
+    );
+  } catch (error) {
+    toast.error("Couldn't like post. Try again!");
+  }
 };
 
 export const dislikePost = async (postID, user) => {
-  await setDoc(
-    doc(allPostsCollection, postID),
-    {
-      likes: arrayRemove({
-        avatar: user.photoURL,
-        displayName: user.displayName,
-        userID: user.uid,
-      }),
-    },
-    { merge: true }
-  );
+  try {
+    await setDoc(
+      doc(allPostsCollection, postID),
+      {
+        likes: arrayRemove({
+          avatar: user.photoURL,
+          displayName: user.displayName,
+          userID: user.uid,
+        }),
+      },
+      { merge: true }
+    );
+  } catch (error) {
+    toast.error("Couldn't dislike post. Try again!");
+  }
 };
 
 export const postComment = async ({ postID }, comment, user) => {
-  await setDoc(
-    doc(allPostsCollection, postID),
-    {
-      comments: arrayUnion({
-        avatar: user.photoURL,
-        displayName: user.displayName,
-        userID: user.uid,
-        comment: comment,
-        date: date,
-      }),
-    },
-    { merge: true }
-  );
+  try {
+    await setDoc(
+      doc(allPostsCollection, postID),
+      {
+        comments: arrayUnion({
+          avatar: user.photoURL,
+          displayName: user.displayName,
+          userID: user.uid,
+          comment: comment,
+          date: date,
+        }),
+      },
+      { merge: true }
+    );
+  } catch (error) {
+    toast.error("Couldn't post the comment. Try again!");
+  }
 };
 
 export const deleteComment = async (post, comment) => {
-  await setDoc(
-    doc(allPostsCollection, post.postID),
-    {
-      comments: arrayRemove(comment),
-    },
-    { merge: true }
-  );
+  try {
+    await setDoc(
+      doc(allPostsCollection, post.postID),
+      {
+        comments: arrayRemove(comment),
+      },
+      { merge: true }
+    );
+  } catch (error) {
+    toast.error("Couldn't delete the comment. Try again!");
+  }
 };
 
 export const followUser = async (currentUser, userToFollow) => {
@@ -137,8 +243,9 @@ export const followUser = async (currentUser, userToFollow) => {
       },
       { merge: true }
     );
+    toast.success(`You are now following ${userToFollow.displayName}`);
   } catch (error) {
-    console.log(error.message);
+    toast.error(`Couldn't follow ${userToFollow.displayName}. Try again!`);
   }
 };
 
@@ -159,49 +266,70 @@ export const unfollowUser = async (currentUser, userToUnfollow) => {
       },
       { merge: true }
     );
+    toast.success(`Unfollowed ${userToUnfollow.displayName}`);
   } catch (error) {
-    console.log(error.message);
+    toast.error(`Couldn't unfollow ${userToUnfollow.displayName}. Try again!`);
   }
 };
 
 export const archivePost = async (post, user) => {
-  await setDoc(
-    doc(collection(db, "users"), user?.uid),
-    {
-      archives: arrayUnion(post),
-    },
-    { merge: true }
-  );
-  deletePost(post);
+  try {
+    await setDoc(
+      doc(collection(db, "users"), user?.uid),
+      {
+        archives: arrayUnion(post),
+      },
+      { merge: true }
+    );
+    deletePost(post);
+    toast.success("Post archived");
+  } catch (error) {
+    toast.error("Couldn't archive post. Try again!");
+  }
 };
 
 export const unarchivePost = async (post, user) => {
-  await setDoc(
-    doc(collection(db, "users"), user?.uid),
-    {
-      archives: arrayRemove(post),
-    },
-    { merge: true }
-  );
-  await setDoc(doc(allPostsCollection, post.postID), post, { merge: true });
+  try {
+    await setDoc(
+      doc(collection(db, "users"), user?.uid),
+      {
+        archives: arrayRemove(post),
+      },
+      { merge: true }
+    );
+    await setDoc(doc(allPostsCollection, post.postID), post, { merge: true });
+    toast.success("Post unarchived");
+  } catch (error) {
+    toast.error("Couldn't unarchive post. Try again!");
+  }
 };
 
 export const bookmarkPost = async (post, user) => {
-  await setDoc(
-    doc(collection(db, "users"), user?.uid),
-    {
-      bookmarks: arrayUnion(post),
-    },
-    { merge: true }
-  );
+  try {
+    await setDoc(
+      doc(collection(db, "users"), user?.uid),
+      {
+        bookmarks: arrayUnion(post),
+      },
+      { merge: true }
+    );
+    toast.success("Post bookmarked");
+  } catch (error) {
+    toast.error("Couldn't bookmark post. Try again!");
+  }
 };
 
 export const undoBookmarkPost = async (post, user) => {
-  await setDoc(
-    doc(collection(db, "users"), user?.uid),
-    {
-      bookmarks: arrayRemove(post),
-    },
-    { merge: true }
-  );
+  try {
+    await setDoc(
+      doc(collection(db, "users"), user?.uid),
+      {
+        bookmarks: arrayRemove(post),
+      },
+      { merge: true }
+    );
+    toast.success("Removed from bookmarks");
+  } catch (error) {
+    toast.error("Couldn't remove from bookmarks. Try again!");
+  }
 };
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage();
 
-export { app, auth, db };
+export { app, auth, db, storage };
